4cffea97c027cde668c889c57303641ea4b2c47a
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-08-14 19:22:33 +0430

    Updates

---
+++ precache-manifest.3e3cc6d737922ffd66a2e2c95595454b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34af89d1974970143c6b327529d606c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5313d339fcb914c0e40",
+    "url": "/static/js/2.9a1b1d89.chunk.js"
+  },
+  {
+    "revision": "d97be893b8c46676b926",
+    "url": "/static/js/main.b113e639.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
