9088b9d667c6eb14cbcd02d0cacc7ad61a475d00
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2024-09-23 19:35:08 +0330

    Fix bunch of issues and some small tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
 import localFont from "next/font/local";
+import Image from "next/image";
+
+import { DockDemo } from "@/components/Dock";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,6 +19,7 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
+      <DockDemo />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
