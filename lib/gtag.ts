8303b82bbd5034ab8b054741607996cfc50e1908
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2024-10-31 23:42:15 +0330

    Fuck google

--- lib/gtag.ts
+++
@@ -1,7 +0,0 @@
-export const GA_TRACKING_ID = "G-08JPE87VVY";
-
-export const pageview = (url: string) => {
-  (window as unknown as { gtag: Function }).gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 import Script from "next/script";
 import { useEffect } from "react";
 
-import { GA_TRACKING_ID, pageview } from "@/lib/gtag";
 import "@/styles/globals.css";
 
 const sansFont = Montserrat({
@@ -19,41 +18,11 @@ const monoFont = Oxygen_Mono({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <div className={`${sansFont.variable} ${monoFont.variable} font-sans`}>
       <Component {...pageProps} />
       <div id="tooltip"></div>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </div>
   );
 }
